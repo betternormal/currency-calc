b42d3c729a71440536627bb3733a928d18cac62f
praconfi <praconfi@gmail.com>
2021-12-20 22:44:36 +0900

    Initial commit

---
+++ src/main/java/com/example/currencycalc/CurrencyCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencycalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/currencycalc/CurrencyCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencycalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
